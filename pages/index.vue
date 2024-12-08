7ff724342446ea7e5760ce71354cd078005b85a9
avtandil <shainidze@gmail.com>
2024-12-08 20:18:37 +0200

    It has added the Kinde auth module

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Welcome to my Nuxt3 app</h1>
+
+    <div v-if="$auth.loggedIn">
+      <NuxtLink to="/api/logout" external> Sign out </NuxtLink>
+    </div>
+
+    <div v-else>
+      <LoginLink to="/api/login" external> Sign in </LoginLink>
+      <RegisterLink to="/api/register" external> Sign up </RegisterLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const auth = useState("auth");
+const loggedIn = auth.value.loggedIn;
+
+useSeoMeta({
+  title: loggedIn ? "Logged in" : "Not logged in",
+});
+</script>
